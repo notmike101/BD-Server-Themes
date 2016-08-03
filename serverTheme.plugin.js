c64a70301d7869a3ddbd0566e5a5a1fdd097b843
Mike Orozco <mike.orozco94@gmail.com>
2016-08-02 20:51:17 -0700

    Multiple changes
    * Rewrite of prototype use
    * Added ability to open files from within client
    * Added check for if theme file exists before attempting to load
    * Changed to v1.0.0

--- serverTheme.plugin.js
+++ serverTheme.plugin.js
@@ -1,51 +1,5 @@
 //META{"name":"serverTheme"}*//
-function serverTheme(){};
-
-/* Variables */
-serverTheme.themePath = process.env.APPDATA + "\\BetterDiscord\\themes\\";
-serverTheme.intervalTimer1 = null;
-
-/* Internal Functions */
-serverTheme.prototype.doesFileExist = function(filePath) {
-    try {
-        require('fs').accessSync(filePath);
-        return true;
-    } catch(e) {
-        return false;
-    }
-};
-serverTheme.prototype.getFileContent = function(filePath,callback) {
-    if(serverTheme.prototype.doesFileExist(filePath)) {
-        var readStream = require('fs').createReadStream(filePath);
-        var buffer = [];
-
-        readStream.on('readable', function(){
-            while ((chunk = readStream.read()) != null) {
-                buffer.push(chunk.toString().replace(/[\r\n]/gim,''));
-            }
-        });
-        readStream.on('end', function(){
-            callback(buffer.join(''));
-            return 0;
-        });
-    } else {
-        callback('');
-        return 0;
-    }
-};
-serverTheme.prototype.injectCSS = function(buffer) {
-    BdApi.clearCSS("serverTheme-css");
-    BdApi.injectCSS("serverTheme-css", buffer);
-}
-serverTheme.prototype.loadServerCSS = function(serverHash) {
-    serverTheme.prototype.getFileContent(serverTheme.themePath + serverHash+'.servertheme.css',serverTheme.prototype.injectCSS);
-};
-serverTheme.prototype.getHashFromURL = function(url) {
-    return url.split('/')[2];
-};
-serverTheme.prototype.getCurrentServerHash = function() {
-    return serverTheme.prototype.getHashFromURL($('.guild.selected a').attr('href'));
-}
+var serverTheme = function(){};
 
 /* Information */
 serverTheme.prototype.getName = function(){
@@ -55,29 +9,80 @@ serverTheme.prototype.getDescription = function(){
     return 'Ability to use specific themes on individual servers.';
 };
 serverTheme.prototype.getVersion = function(){
-    return '0.8';
+    return '1.0.0';
 };
 serverTheme.prototype.getAuthor = function(){
     return '<a href="https://github.com/IRDeNial" target="_BLANK">DeNial</a>';
 };
 
-/* API Hooks */
-serverTheme.prototype.load = function(){};
+serverTheme.prototype.load = function(){
+    /* Variables */
+    this.themePath = process.env.APPDATA + "\\BetterDiscord\\themes\\";
+    this.intervalTimer1 = null;
+
+    /* Functions */
+    this.loadServerCSS = function(serverHash) {
+        this.getFileContent(this.themePath + serverHash+'.servertheme.css',this.injectCSS);
+        console.log("Injected theme for server " + serverHash);
+        $('#serverTheme-css').addClass('theme-'+serverHash);
+    };
+    this.getCurrentServerHash = function() {
+        return $('.guild.selected a').attr('href').split('/')[2];
+    };
+    this.injectCSS = function(buffer) {
+        BdApi.clearCSS("serverTheme-css");
+        BdApi.injectCSS("serverTheme-css", buffer);
+    };
+    this.getFileContent = function(filePath,callback) {
+        if(this.doesFileExist(filePath)) {
+            var readStream = require('fs').createReadStream(filePath);
+            var buffer = [];
+
+            readStream.on('readable', function(){
+                while ((chunk = readStream.read()) != null) {
+                    buffer.push(chunk.toString().replace(/[\r\n]/gim,''));
+                }
+            });
+            readStream.on('end', function(){
+                callback(buffer.join(''));
+                return 0;
+            });
+        } else {
+            callback('');
+            return 0;
+        }
+    };
+    this.doesFileExist = function(filePath) {
+        try {
+            require('fs').accessSync(filePath);
+            return true;
+        } catch(e) {
+            return false;
+        }
+    };
+};
 serverTheme.prototype.unload = function(){};
 serverTheme.prototype.stop = function(){
     BdApi.clearCSS("serverTheme-css");
 };
 serverTheme.prototype.onSwitch = function(){
-    serverTheme.prototype.loadServerCSS(serverTheme.prototype.getCurrentServerHash());
+    this.loadServerCSS(this.getCurrentServerHash());
 };
 serverTheme.prototype.start = function(){
-    serverTheme.prototype.loadServerCSS(serverTheme.prototype.getCurrentServerHash());
-   
-    /*
+    this.loadServerCSS(this.getCurrentServerHash());
+
     $('.guild-header ul').prepend('<li><a class="server-css">Server CSS</a></li>');
 
     $('.guild-header ul .server-css').click(function(){
-        serverTheme.prototype.openEditor(serverTheme.themePath + serverTheme.prototype.getCurrentServerHash() +'.servertheme.css');
+        var filePath = process.env.APPDATA + "\\BetterDiscord\\themes\\" + $('.guild.selected a').attr('href').split('/')[2] + '.servertheme.css';
+        
+        try {
+            require('fs').accessSync(filePath);
+        } catch(e) {
+            require('fs').closeSync(require('fs').openSync(filePath, 'w'));
+        }
+        
+        require('child_process').exec('start "" "' + filePath +'"');
+        
     });
-    */
 };
